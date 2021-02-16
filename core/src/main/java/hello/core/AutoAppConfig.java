e55f836ab9a449455d0c070291d6a164a9653d09
Kim Junyoung <orsay0827@gmail.com>
2021-02-17 00:28:19 +0900

    Study Spring-Core

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // 예제를 안전하기 유지하기 위해 Configuration을 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,11 +1,14 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 테스트용 MemoryMemberRepository
  */
+@Component
 public class MemoryMemberRepository implements MemberRepository{
     // 동시성 이슈가 있어 ConcurrentHashMap 을 사용해야 하지만... 교육이니까
     private  static Map<Long, Member> store = new HashMap<>();
