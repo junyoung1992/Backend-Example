a4ec9578df8f20b70e86f1ff8718a60c5dc39080
Kim Junyoung <orsay0827@gmail.com>
2021-06-02 23:30:36 +0900

    Study Spring Boot & RESTAPI

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Lombok
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+
+    private String message;
+
+}
