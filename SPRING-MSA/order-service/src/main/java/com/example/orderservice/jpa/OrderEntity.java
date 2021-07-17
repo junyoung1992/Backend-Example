d9c7c398a8ee4d82df343371fc3c90d6a69b5177
Kim Junyoung <orsay0827@gmail.com>
2021-07-18 00:19:03 +0900

    Study Spring MSA - KAFKA

--- SPRING-MSA/order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
+++ SPRING-MSA/order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -12,6 +12,18 @@ import java.util.Date;
 @Table(name = "orders")
 public class OrderEntity implements Serializable {
 
+    /**
+     * create table orders
+     * (id int auto_increment primary key,
+     *  user_id varchar(50) not null,
+     *  product_id varchar(20) not null,
+     *  order_id varchar(50) not null,
+     *  qty int default 0,
+     *  unit_price int default 0,
+     *  total_price int default 0,
+     *  created_at datetime default now());
+     */
+
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
