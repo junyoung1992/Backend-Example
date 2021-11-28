e2161420780bf38a1a26ae0434ed8391aae4c8e1
Kim Junyoung <orsay0827@gmail.com>
2021-11-28 20:49:54 +0900

    DJANGO-TUTORIAL: Complete tutorial

--- DJANGO-TUTORIAL/mysite/polls/admin.py
+++ DJANGO-TUTORIAL/mysite/polls/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 
-admin.site.register(Question)
+# admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3   # 기본으로 3가지 선택 항목을 제공
+    
+    
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    
+admin.site.register(Question, QuestionAdmin)
--- DJANGO-TUTORIAL/mysite/polls/views.py
+++ DJANGO-TUTORIAL/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -35,8 +36,14 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
+
 
 """
 def detail(request, question_id):
@@ -61,6 +68,13 @@ def detail(request, question_id):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 """
 def results(request, question_id):
@@ -70,6 +84,7 @@ def results(request, question_id):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
