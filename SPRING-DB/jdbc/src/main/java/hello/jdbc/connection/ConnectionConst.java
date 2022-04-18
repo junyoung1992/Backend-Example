4372abee165f9fee9e476bad900fcf3b0228f9b9
Kim Junyoung <orsay0827@gmail.com>
2022-04-18 23:54:25 +0900

    Study JDBC

---
+++ SPRING-DB/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/dummy?serverTimezone=Asia/Seoul";
+    public static final String USERNAME = "dummy";
+    public static final String PASSWORD = "dummy";
+
+}
