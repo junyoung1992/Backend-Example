f25340e1a98045bd1bdcc7f1a2292fa073f36b1e
Kim Junyoung <orsay0827@gmail.com>
2022-05-17 00:56:01 +0900

    Study refactoring

--- Refactoring/refactoring/src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ Refactoring/refactoring/src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -13,6 +13,12 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
+//        assert discountRate != null && discountRate > 0;
+        // assertion 문이 예외처리 기능을 해줄 수 없다.
+        // 실제 프로그램 실행될 때는 동작하지 않는다.
+        if (discountRate == null || discountRate <= 0) {
+            throw new IllegalArgumentException(discountRate + " can't be minus.");
+        }
         this.discountRate = discountRate;
     }
 }
