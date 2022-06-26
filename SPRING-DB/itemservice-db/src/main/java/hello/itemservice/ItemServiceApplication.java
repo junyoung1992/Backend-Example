e342e67d8a486be84f480a0eb8ad7b5049592b3a
Kim Junyoung <orsay0827@gmail.com>
2022-06-27 00:56:47 +0900

    Study Spring-DB (MyBatis)

--- SPRING-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SPRING-DB/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MybatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 //@Slf4j
-@Import(JdbcTemplateV3Config.class)
+@Import(MybatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
